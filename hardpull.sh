a2cbe3761337c13669bc2ac35cbcb2223298ca39
Jim Hessin <jhessin@gmail.com>
2025-09-27 14:14:11 -0600

    2025-09-27

--- hardpull.sh
+++ hardpull.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 git reset --hard
-git pull
+git fetch origin
+git checkout origin/main
+git branch -d main
+git checkout -b main origin/main
--- hardpush.sh
+++ hardpush.sh
@@ -2,4 +2,5 @@
 
 git add .
 git commit -m $(date +%Y-%m-%d)
-git push --force
+git push
+# git push --force
